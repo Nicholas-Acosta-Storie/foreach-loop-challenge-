ee3942d4d77d8da0d2ce630b6839532f41b5e841
nicho <nacostastorie@cnm.edu>
2021-10-27 09:02:22 -0600

    for each loop challenge

---
+++ script.js
@@ -0,0 +1,20 @@
+let people = [{
+    name:'bob'
+
+},{
+    name:'jim'
+},{
+    name:'Matt'
+
+},{
+    name:'Mary'
+},{
+    name:'Susy'
+},{
+    name:'Sarah'
+}]
+console.log(people[1])
+
+people.forEach(person => person)
+
+console.log(people)
